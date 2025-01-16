dd7cc88647b486e1d0f88b8bfe7222f5153ff4ff
antonistorb <antonistorb@yahoo.com>
2025-01-16 18:45:21 +0200

    Changes:
    - Updated workflow to trigger only on tag pushes. Alpha/beta publish to `test.pypi`, regular to `pypi`.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # This is mainly for testing changes without having to package and install.
 
-from src.frequensee import main_cli
 import time
+
+from src.frequensee import main_cli
 
 
 if __name__ == "__main__":
